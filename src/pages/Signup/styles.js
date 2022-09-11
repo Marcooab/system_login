c988cfd7a59c15860b120d802218ca57fd5b9a42
Marcooab <marcooab2012@hotmail.com>
2022-09-11 21:00:39 +0100

    Implementando GIT

---
+++ src/pages/Signup/styles.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex; 
+    align-items: center; 
+    justify-content: center; 
+    flex-direction: column;
+    gap: 10px;
+    height: 100vh; 
+`;
+
+export const Content = styled.div`
+    display: flex; 
+    align-items: center; 
+    justify-content: center; 
+    flex-direction: column;
+    gap: 15px;
+    width: 100%;
+    max-width: 350px;
+    padding: 20px; 
+    border-radius: 5px; 
+    box-shadow: 0 1px 2px #0003;
+    background-color: white;
+`;
+
+export const Label = styled.label`
+    font-size: 18px; 
+    font-weight: 600; 
+    color: #676767
+`;
+
+export const LabelSignin = styled.label`
+    font-size: 16px; 
+    color: #676767`
+
+export const labelError = styled.label`
+    font-size: 14px; 
+    color: red; 
+`
+export const Strong = styled.strong`
+    cursor: pointer; 
+    
+    a {
+        text-decoration: none; 
+        color: #676767
+    }
+`;
